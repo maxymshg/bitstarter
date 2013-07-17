6803c07537d806464bfdeb2b2378d23c10138302
Maxim Shekhovets <maxymshg@gmail.com>
2013-07-17 04:42:44 +0000

    web.js was updated

--- web.js
+++ web.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var fs = require('fs');
 
-var app = express.createServer(express.logger());
+var app = express();
+app.use(express.logger());
 
 app.get('/', function(request, response) {
   response.send(
@@ -10,7 +11,7 @@ app.get('/', function(request, response) {
   );
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
